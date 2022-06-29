2a85549a34c92bdc1720204d69dedde1cf6308d0
Anug Saha <anugsaha@gmail.com>
2022-06-29 15:28:51 -0400

    fix looping issue with scanner

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,72 +2,64 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Boolean value = true; 
+        Boolean value = true;
         try (Scanner scanner = new Scanner(System.in)) {
             while (value) {
                 System.out.println("Would you like to ADD a Book(1) or Look up an Existing Book(2)?");
 
                 String userInput = scanner.nextLine();
                 if (userInput.equals("1")) {
-                    getBookInput();
-                    value = false; 
-                } else if (userInput.equals("2")){
-                    lookUpBook();
-                    value = false; 
+                    getBookInput(scanner);
+
+                } else if (userInput.equals("2")) {
+                    lookUpBook(scanner);
+
                 }
-                
-                value = true; 
             }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        } 
-
-    }
-
-    public static void getBookInput() {
-        try (Scanner scanner = new Scanner(System.in)) {
-
-            String title = "";
-            String genre = "";
-            Integer numberOfPages = 0;
-            System.out.println("Add a Book");
-
-            System.out.print("Enter title: ");
-            title = scanner.nextLine();
-            System.out.print("Enter the Genre: ");
-            genre = scanner.nextLine();
-
-            System.out.print("Please enter the number of pages: ");
-            numberOfPages = scanner.nextInt();
-            scanner.nextLine();
-
-            Book book = new Book(title, genre, numberOfPages);
-            Library.addBook(title, book);
 
         } catch (Exception e) {
             e.printStackTrace();
         }
+ 
+    }
+
+    public static void getBookInput(Scanner scanner) {
+
+        String title = "";
+        String genre = "";
+        Integer numberOfPages = 0;
+        System.out.println("Add a Book");
+
+        System.out.print("Enter title: ");
+        title = scanner.nextLine();
+        System.out.print("Enter the Genre: ");
+        genre = scanner.nextLine();
+
+        System.out.print("Please enter the number of pages: ");
+        numberOfPages = scanner.nextInt();
+        scanner.nextLine();
+
+        Book book = new Book(title, genre, numberOfPages);
+        Library.addBook(title, book);
 
     }
 
-    public static void lookUpBook() {
-        try (Scanner scanner = new Scanner(System.in)) {
+    public static void lookUpBook(Scanner scanner) {
 
-            String title = "";
-            System.out.print("Enter the title of the book you are looking for: ");
-            title = scanner.nextLine();
+        String title = "";
+        System.out.print("Enter the title of the book you are looking for: ");
+        title = scanner.nextLine();
 
-            Book book = Library.showLibraryCollection().get(title);
+        Book book = Library.showLibraryCollection().get(title);
 
-            if (book != null) {
-                System.out.println(book);
-            } else {
-                System.out.println("Sorry that book is not in our library");
-            }
+        if (book != null) {
+            System.out.println(book);
+        } else {
+            System.out.println("Sorry that book is not in our library");
+        }
 
-        } catch (Exception e) {
-            e.printStackTrace();
+        if (scanner.hasNextLine()) {
+            scanner.nextLine();
         }
     }
 }
