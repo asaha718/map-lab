8bcfa048f488671d6251f1586d4bc68e6f3be7cf
Anug Saha <anugsaha@gmail.com>
2022-06-29 17:11:02 -0400

    complete

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -2,7 +2,6 @@ import java.util.HashMap;
 
 public class Library {
     private static HashMap<String, Book> collectionOfBooks = new HashMap<>();
-    private static HashMap<String, Book> collectionOfBooks = new HashMap<>();
 
     public static HashMap<String, Book> showLibraryCollection() {
         return Library.collectionOfBooks;
